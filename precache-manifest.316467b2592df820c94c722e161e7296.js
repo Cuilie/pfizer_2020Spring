f19c3a4a359c5215ab232771c59382d5d6c6764e
Yixian Huang <yhuang5@wpi.edu>
2020-05-12 08:04:27 -0400

    Updates

---
+++ precache-manifest.316467b2592df820c94c722e161e7296.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e648bc8c407622cca16c588a40dc0bbf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cca4b97bb63e10398d5d",
+    "url": "./static/css/main.2e2fffae.chunk.css"
+  },
+  {
+    "revision": "9be7e0afbc073e9059f4",
+    "url": "./static/js/2.268033ed.chunk.js"
+  },
+  {
+    "revision": "1166da0ae46b72168c0a8b1fdeb99445",
+    "url": "./static/js/2.268033ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cca4b97bb63e10398d5d",
+    "url": "./static/js/main.fa21308e.chunk.js"
+  },
+  {
+    "revision": "f12b438e4b4da957b82f",
+    "url": "./static/js/runtime-main.e4848c00.js"
+  },
+  {
+    "revision": "1e17c32bc188fa12c86ca611804ee35f",
+    "url": "./static/media/WPILogo.1e17c32b.png"
+  },
+  {
+    "revision": "b97c062cf3f5c3eb25b7b123f1ce4dfc",
+    "url": "./static/media/pfizer2.b97c062c.png"
+  },
+  {
+    "revision": "8c8dc313f2a2be86318076e88daf0889",
+    "url": "./static/media/pfizer3.8c8dc313.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ceeac1860aa07e4957303f5d1b778d25.js"
+  "./precache-manifest.316467b2592df820c94c722e161e7296.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
